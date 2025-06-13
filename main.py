6385bc1b18437428935adbe52db39053012f8cd0
Rangga Priago <141876195+RPriago@users.noreply.github.com>
2025-06-13 21:32:30 +0700

    nine commit

--- main.py
+++ main.py
@@ -41,9 +41,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173",
         "https://frontend-mechine-learning-pi.vercel.app",
-        "https://*.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
